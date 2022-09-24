e87ac62cd2aa24870ebb3fc4ff45baa9d1b7c5da
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-09-24 11:54:45 +0530

    Add files via upload

---
+++ operators/operator/operator.js
@@ -0,0 +1,98 @@
+// addition operator//
+let x = 5;
+let y = 2;
+let z = x + y;
+console.log(z);
+//string in operator//
+let text1 = "1";
+let text2 = "sanker";
+let text3 = text1 + " " + text2;
+console.log(text3);
+//substraction operator//
+let a = 9999000;
+let b = 5688;
+let c = a-b;
+console.log(c);
+//string in substraction//
+let text4 = "Ramm";
+let text5 = "sanker";
+let text6 = text4 - " " -text5;
+console.log(text6);
+//multiplication//
+let d = 9999000;
+let e = 568890;
+let f = d*e;
+console.log(f);
+//division //
+let g = 1000;
+let h = 50;
+let i = g/=h;
+console.log(i);
+//remainder//
+let j = 7;
+let k = 20;
+let l = k %j;
+console.log(l);
+//comparion operator//
+//equal//
+let num = 5;
+let numb= "5";
+let  number = num==numb;
+console.log( number );
+//strict equal//
+//not equal//
+let num1 = 10;
+let numb1= "10";
+let  number1 = num1!=numb1;
+console.log( number1 );
+//strict not equal//
+let num2 = "10";
+let numb2= "10";
+let  number2 = num2!=numb2;
+console.log( number2 );
+//greater than//
+let num3 = "100";
+let numb3= "10";
+let  number3 = num3>numb3;
+console.log( number3 );
+//lesser than//
+let num4 = "100";
+let numb4= "10";
+let  number4 = num4<numb4;
+console.log( number4);
+// ----//LOGIC OPERATOR//-----
+//AND//
+let num5 = 100;
+let numb5= 100;
+let xn=(num5< 1000 && numb5 > 1000) ;
+console.log(xn);
+//or//
+let num6 = 100;
+let numb6= 100;
+let n=(num6< 10 || numb6 > 102) ;
+console.log(n);
+//!not//
+let num7 = 100;
+let numb7= 100;
+let t=!(num7< 10 === numb7 > 90) ;
+console.log(t);
+//Arithmetic operator//
+//increment//
+let num8 = 100;
+let numb8= 100;
+console.log(num8 ++);
+console.log(num8 );
+//decrement//
+let num9 = 5;
+let numb9= 1;
+console.log(num9 ++);
+console.log(num9 );
+//exponentation//
+let num10 = 2;
+let numb10= 2;
+let m=(num10 ** numb10);
+console.log(m );
+
+let x1="8";
+x1===8;
+
